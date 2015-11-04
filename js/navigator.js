638a69e491ed69a96b5ca0be9b11c0e082a1b362
kylehind <kyle@liquidbronze.com>
2015-11-04 12:27:03 +0000

    add padding tobottom of pages

--- js/navigator.js
+++ js/navigator.js
@@ -4,46 +4,50 @@ $(document).ready(function(){
 	$("#main_container").show();
 
 	$(".blog_link").click(function(){
-		$("#get_involved_container").hide();
 		$("#blog_container").show();
-		$("#main_container").hide();
 		$(".underlined_white").removeClass("underlined_white");
 		$(this).addClass("underlined_white")
 		$("#get_involved_container").animate({'left':'-200%'});
 		$("#main_container").animate({'left':'-200%'});
-		$("#blog_container").animate({'left':'0'});
+		$("#blog_container").animate({'left':'0'}, function(){
+			$("#get_involved_container").hide();
+			$("#main_container").hide();
+		});		
 	});
 
 	$(".about_link").click(function(){
-		$("#get_involved_container").hide();
-		$("#blog_container").hide();
 		$("#main_container").show();
 		$(".underlined_white").removeClass("underlined_white");
 		$(this).addClass("underlined_white")
 		$("#get_involved_container").animate({'left':'-200%'});
 		$("#blog_container").animate({'left':'-200%'});
-		$("#main_container").animate({'left':'0'});
+		$("#main_container").animate({'left':'0'}, function(){
+			$("#get_involved_container").hide();
+			$("#blog_container").hide();
+		});
 	});
 
 	$(".get_involved_link").click(function(){
 		$("#get_involved_container").show();
-		$("#blog_container").hide();
-		$("#main_container").hide();
 		$(".underlined_white").removeClass("underlined_white");
 		$(this).addClass("underlined_white")
 		$("#main_container").animate({'left':'-200%'});
 		$("#blog_container").animate({'left':'-200%'});
-		$("#get_involved_container").animate({'left':'0'});
+		$("#get_involved_container").animate({'left':'0'}, function(){
+			$("#blog_container").hide();
+			$("#main_container").hide();
+		});		
 	});
 
 	$(".green_get_involved_link").click(function(){
 		$("#get_involved_container").show();
-		$("#blog_container").hide();
-		$("#main_container").hide();
 		$(".underlined_white").removeClass("underlined_white");
 		$(".get_involved_link").addClass("underlined_white")
 		$("#main_container").animate({'left':'-200%'});
 		$("#blog_container").animate({'left':'-200%'});
-		$("#get_involved_container").animate({'left':'0'});
+		$("#get_involved_container").animate({'left':'0'}, function(){
+			$("#blog_container").hide();
+			$("#main_container").hide();
+		});	
 	});
 });
