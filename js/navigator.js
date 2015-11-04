a75d1a6c4b7d293a06eb18abb4ec487649902012
kylehind <kyle@liquidbronze.com>
2015-11-04 11:59:27 +0000

    sytle navigation better

--- js/navigator.js
+++ js/navigator.js
@@ -1,13 +1,33 @@
 $(document).ready(function(){
 	$(".blog_link").click(function(){
-		console.log("click");
+		$(".underlined_white").removeClass("underlined_white");
+		$(this).addClass("underlined_white")
+		$("#get_involved_container").animate({'left':'-200%'});
 		$("#main_container").animate({'left':'-200%'});
 		$("#blog_container").animate({'left':'0'});
 	});
 
 	$(".about_link").click(function(){
-		console.log("click");
+		$(".underlined_white").removeClass("underlined_white");
+		$(this).addClass("underlined_white")
+		$("#get_involved_container").animate({'left':'-200%'});
 		$("#blog_container").animate({'left':'-200%'});
 		$("#main_container").animate({'left':'0'});
+	});
+
+	$(".get_involved_link").click(function(){
+		$(".underlined_white").removeClass("underlined_white");
+		$(this).addClass("underlined_white")
+		$("#main_container").animate({'left':'-200%'});
+		$("#blog_container").animate({'left':'-200%'});
+		$("#get_involved_container").animate({'left':'0'});
+	});
+
+	$(".green_get_involved_link").click(function(){
+		$(".underlined_white").removeClass("underlined_white");
+		$(".get_involved_link").addClass("underlined_white")
+		$("#main_container").animate({'left':'-200%'});
+		$("#blog_container").animate({'left':'-200%'});
+		$("#get_involved_container").animate({'left':'0'});
 	});
 });
